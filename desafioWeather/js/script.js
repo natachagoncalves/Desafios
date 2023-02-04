457d8461ac2a2a2dcedec8916a77678a83cfa123
Natacha Gonçalves <natachagoncalvesjobs@gmail.com>
2023-02-03 23:50:02 -0200

    css change and js validation

--- desafioWeather/js/script.js
+++ desafioWeather/js/script.js
@@ -1,9 +1,9 @@
-let resultado = document.querySelector(".resultado"); 
-let apiKey = "18364ebabe34dbb09ff4fa05d5f98bb4";  
-
 //variáveis e seleção de elementos
+const resultado = document.querySelector(".resultado"); 
+const apiKey = "18364ebabe34dbb09ff4fa05d5f98bb4";  
 const apiCountryURL = "https://countryflagsapi.com/png/";  
-
+const requi = document.querySelector(".required")
+const spans = document.querySelector('.span-requirid')
 const inputLugar = document.querySelector("#input-lugar");
 const btnBuscar = document.querySelector("#btn-buscar");
 const dadosWeather = document.querySelector(".dados-weather");
@@ -45,22 +45,42 @@ const mostrarDadosWeather = async (lugar) => {
 
 }
 
-//eventos
-btnBuscar.addEventListener("click", (event) => {
-    event.preventDefault();
-
-    let temp = tempElementC.style.display = "block"
-    let dados = dadosWeather.style.display = "block" 
-    tempElementC.style.fontSize = "28px"
-
+function mostrarTemp() {
+    let temp = tempElementC.style.display = "block";
+    let tempF = tempElementF.style.display = "none"
+    let dados = dadosWeather.style.display = "block" ;
+    tempElementC.style.fontSize = "28px";
     let lugar = inputLugar.value;
     mostrarDadosWeather(lugar);
+}
+
+function setError() {
+    requi.style.border = '2px solid #8d0f0f';
+    spans.style.display = 'block';
+}
+
+function removeError() {
+    requi.style.border = '';
+    spans.style.display = 'none';
+}
+
+
+//eventos
+
+btnBuscar.addEventListener("click", (event) => {
+    event.preventDefault();
+    if(requi.value.length == 0 ){
+        setError();
+    } else {
+        removeError();
+        mostrarTemp();
+    }
 }) 
+
 
 inputLugar.addEventListener("keyup", (event) => {
     if(event === "Enter") {
         let lugar = event.target.value;
-        
         mostrarDadosWeather(lugar);
     }
 })
@@ -68,6 +88,7 @@ inputLugar.addEventListener("keyup", (event) => {
 grauC.addEventListener("click", () => {
     let temp2 = tempElementC.style.display = "block"
     let tempF = tempElementF.style.display = "none"
+    tempElementC.style.fontSize = "28px";
 }) 
 
 grauF.addEventListener("click", () => {
